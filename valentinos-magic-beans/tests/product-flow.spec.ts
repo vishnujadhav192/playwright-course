d44db64b84fe7e26b07f951cbae320b4fa8933cc
vishnujadhav192 <vishnujadhav192@gmail.com>
2026-09-12 00:48:06 +0530

    Complete workflow for product order - without steps

--- valentinos-magic-beans/tests/product-flow.spec.ts
+++ valentinos-magic-beans/tests/product-flow.spec.ts
@@ -19,42 +19,31 @@ test('Item is added to the shopping cart', async ({ page }) => {
 
 })
 
-test('Complete workflow for product order - with steps', async ({ page }) => {
+test('Complete workflow for product order', async ({ page }) => {
     await page.goto('/products');
 
-    let addedProduct: Awaited<ReturnType<typeof products.addProductToCart>> = {} as any;
+    const addedProduct = await products.addProductToCart(page, 1);
 
-    await test.step('add product to cart', async () => {
-        addedProduct = await products.addProductToCart(page, 1);
-    })
+    await page.locator('[data-test-id="header-cart-button"]').getByRole('button').click();
 
-    await test.step('go to checkout page', async () => {
-        await page.locator('[data-test-id="header-cart-button"]').getByRole('button').click();
-        await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
-    })
+    await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
 
-    await test.step('complete checkout information', async () => {
-        await checkout.addContactInfo(page)
-        await checkout.addPaymentInfo(page)
-        await checkout.addShippingAddress(page)
-        await checkout.placeOrder(page)
-    })
+    await checkout.addContactInfo(page)
+    await checkout.addPaymentInfo(page)
+    await checkout.addShippingAddress(page)
+    await checkout.placeOrder(page)
 
-    let orderId: string | null;
+    // get orderId:
+    const orderWrapper = page.getByText('Your Order ID is:').locator('..')
+    const orderId = await orderWrapper.getByRole('paragraph').nth(1).textContent()
 
-    await test.step('get the orderID', async () => {
-        const orderWrapper = page.getByText('Your Order ID is:').locator('..')
-        orderId = await orderWrapper.getByRole('paragraph').nth(1).textContent()
-    })
+    // open the contact page:
+    await page.getByRole('button', { name: 'Track Your Order' }).click();
+    await contact.fillOrderIdAndEmail(page, orderId!, checkout.testValues.email)
+    await contact.clickTrackOrder(page)
 
-    await test.step('open the contact page', async () => {
-        await page.getByRole('button', { name: 'Track Your Order' }).click();
-        await contact.fillOrderIdAndEmail(page, orderId!, checkout.testValues.email)
-        await contact.clickTrackOrder(page)
-    })
+    // check if ordered item is returned:
+    const firstOrder = page.getByText(addedProduct.name!)
+    await expect(firstOrder).toBeVisible()
 
-    await test.step('check if ordered item is returned', async () => {
-        const firstOrder = page.getByText(addedProduct.name!)
-        await expect(firstOrder).toBeVisible()
-    })
 })
